0157ccf79d7aa1a344ad1f84326c2a097c02bafd
deepakphpweb <dipakrana2760@gmail.com>
2021-11-29 21:05:33 +0530

    Added : Create Reverse nubmer by ReverseNumByUsingRecursion

--- src/com/bridgelabz/JavaProgramDaySix.java
+++ src/com/bridgelabz/JavaProgramDaySix.java
@@ -3,17 +3,25 @@ import java.util.Scanner;
 
 public class JavaProgramDaySix {
 
-    public static void main(String[] args) {
-
-        int number = 987654, reverse = 0;
-        while(number != 0)
-        {
-            int remainder = number % 10;
-            reverse = reverse * 10 + remainder;
-            number = number/10;
+    //method for reverse a number
+    public static void reverseNumber(int number) {
+        if (number < 10) {
+            //prints the same number if the number is less than 10
+            System.out.println(number);
+            return;
+        } else {
+            System.out.print(number % 10);
+            reverseNumber(number / 10);
         }
-        System.out.println("The reverse of the given number is: " + reverse);
+    }
+        public static void main(String[] args) {
 
+        System.out.print("Enter the number that you want to reverse: ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.print("The reverse of the given number is: ");
+        //method calling
+        reverseNumber(num);
 
     }
 }
