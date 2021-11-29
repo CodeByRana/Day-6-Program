359b78086c0156b3531cf8a70de6ac2f21f7a294
deepakphpweb <dipakrana2760@gmail.com>
2021-11-29 20:34:27 +0530

    Added : created a fibonacci Series with print message

--- src/com/bridgelabz/JavaProgramDaySix.java
+++ src/com/bridgelabz/JavaProgramDaySix.java
@@ -4,6 +4,9 @@ public class JavaProgramDaySix {
 
     public static void main(String[] args) {
 
+
+        System.out.println("Welcome To Java Program");
+
         int n1=0,n2=1,n3,i,count=10;
         System.out.print(n1+" "+n2);    //printing 0 and 1
 
